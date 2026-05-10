401e25c654475d8067bbe4da1ee4a39f19a95c4e
Shruti Sharma <shrutisharma19032@gmail.com>
2026-05-10 23:46:41 +0530

    your message

---
+++ src/sttringBasics/StringBasics.java
@@ -0,0 +1,10 @@
+package sttringBasics;
+
+public class StringBasics {
+    static void main(String[] args) {
+        char[] arr= {'s', 'h', 'r', 'u', 't', 'i'};
+        for(char ele: arr);
+
+
+    }
+}
