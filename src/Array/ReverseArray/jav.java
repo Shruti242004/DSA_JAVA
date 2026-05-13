30cd2bac75c0e4b42093ce259baf442dfdfac683
Shruti Sharma <shrutisharma19032@gmail.com>
2026-05-13 22:32:05 +0530

    updated feature

---
+++ src/Array/ReverseArray/jav.java
@@ -0,0 +1,21 @@
+package Array.ReverseArray;
+
+public class jav {
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 19, 56, 9, 83, 18, 24, 85, 14};
+        int n = arr.length;
+        int i = 0;
+
+        for(int j = n - 1; i < j; --j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            ++i;
+        }
+
+        for(int ele : arr) {
+            System.out.print(ele + " ");
+        }
+
+    }
+}
